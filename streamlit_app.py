24a51e9e66fa9644948f79d0e30eb782160da6f3
Jefferson Ramos <151884695+Jeffracu@users.noreply.github.com>
2023-12-15 05:14:18 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,97 @@
-import altair as alt
-import numpy as np
+## ⚙️ Setup
+
+import os
+import openai
+import warnings
 import pandas as pd
+import os
 import streamlit as st
 
-"""
-# Welcome to Streamlit!
+warnings.filterwarnings("ignore")
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+# Obtén la openai api key desde https://platform.openai.com/account/api-keys 🔑
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+from langchain.agents import AgentType
+from langchain_experimental.agents.agent_toolkits import create_pandas_dataframe_agent
+from langchain.chat_models import ChatOpenAI
+from langchain.document_loaders.csv_loader import CSVLoader
+from langchain.prompts.prompt import PromptTemplate
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+# Título de la página
+st.set_page_config(page_title='🤖 Structural Database Search')
+st.title('🤖 DataBase-SearchGPT: Asistente de Búsqueda en la Base de datos')
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
+##Primero se deben subir los archivos csv que se van a utilizar como fuente de información 
 
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
+# Lista de archivos csv
+archivos_csv = ['/content/drive/MyDrive/Colab Notebooks/structuraldbChat/informacionEstructuras.csv', "/content/drive/MyDrive/Colab Notebooks/structuraldbChat/archivosMemorias.csv"]
 
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
+# Crear una lista vacía para almacenar los dataframes
+lista_dataframes = []
 
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+# Iterar sobre la lista de archivos csv
+for archivo in archivos_csv:
+    # Leer cada archivo csv y agregarlo a la lista de dataframes
+    lista_dataframes.append(pd.read_csv(archivo))
+
+# Usar el primer DataFrame como base
+df_base = lista_dataframes[0]
+
+# Iterar sobre los demás DataFrames y combinarlos con el DataFrame base
+for df in lista_dataframes[1:]:
+    df_base = pd.merge(df_base, df, on='id_archivo')
+
+# Añadir una opción para seleccionar el modelo de openai a utilizar
+model_option = st.selectbox(
+    'Selecciona un modelo:',
+    ('gpt-3.5-turbo-1106','gpt-4','gpt-4-1106-preview')
+)
+
+def generate_response(df_base, user_query):
+ # Crear un objeto `ChatOpenAI` con la configuración deseada.
+ llm = ChatOpenAI(temperature=0, model=model_option, openai_api_key=openai_api_key, streaming=True)
+ df_base = df_base
+ # Crear un objeto `PromptTemplate` con el formato de la respuesta deseada.
+ _DEFAULT_TEMPLATE = """
+  Dada una consulta del usuario {dialect}
+Sólo utiliza la información de df_base
+  1. Consulta los datos similares en df_base
+  2. Devuelve una respuesta en español que incluya:
+     - Referencias a proyectos con las condiciones exactas a la consulta de usuario en df_base con id_archivo
+     - Referencias a proyectos con las condiciones similares a la consulta de usuario en df_base con id_archivo
+
+  Respuesta {output}
+  Pregunta {input}
+  """
+ PROMPT = PromptTemplate(input_variables=["input","dialect","output",],template=_DEFAULT_TEMPLATE)
+  # Crear un agente `pandas_df_agent` que usa el modelo de lenguaje y el DataFrame base.
+ pandas_df_agent = create_pandas_dataframe_agent(
+        llm,
+        df_base,
+        verbose=True,
+        agent_type=AgentType.OPENAI_FUNCTIONS,
+        prompt_template=PROMPT,
+        max_iterations=5,
+        handle_parsing_errors=True,
+  )
+ 
+ response = pandas_df_agent({"input": user_query}, {"dialect": _DEFAULT_TEMPLATE}, include_run_info=True)
+ result = generate_response(df_base, user_query)
+ return st.success(result)
+
+
+## Obtén las características de la estructura del usuario
+caracteristicas_estructura = st.text_input('Ingresa las características para la búsqueda:', placeholder = 'Escribe tu consulta aquí ...')
+
+# Agrega más información a la solicitud para una respuesta robusta
+texto_ad1 = "Limítate a siempre actuar como buscador en la base de datos, además referencia proyectos usando id_archivo con las condiciones exactas en df_base a la siguiente consulta de usuario :\n"
+texto_ad2 = "\n O referencia proyectos usando id_archivo con alguna condición similar a la consulta de usuario en df_base y describe las características de cada proyecto referenciado"
+user_query = texto_ad1 + caracteristicas_estructura + texto_ad2
+
+openai_api_key = st.text_input('Inserte su OpenAI API Key.\nO utilice la siguiente con saldo disponible de prueba: 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', type='password', disabled=not (user_query))
+#"""Análisis del agente"""
+if not openai_api_key.startswith('sk-'):
+ st.warning('Por favor ingrese su llave OpenAI API!', icon='⚠')
+if openai_api_key.startswith('sk-'):
+ st.header('Sugerencia')
+ generate_response(df_base, user_query)
