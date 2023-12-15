dcec127d8a17b9399f7329e6400befccc1012201
Jefferson Ramos <151884695+Jeffracu@users.noreply.github.com>
2023-12-15 08:35:39 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,23 +25,8 @@ st.title('🤖 DataBase-SearchGPT: Asistente de Búsqueda en la Base de datos')
 url1 = "https://github.com/Jeffracu/DataBase-SearchGPT/blob/master/informacionEstructuras.csv"
 url2 = "https://github.com/Jeffracu/DataBase-SearchGPT/blob/master/archivosMemorias.csv"
 
-
-# Lista de archivos csv
-archivos_csv = (url1, url2)
-# Crear una lista vacía para almacenar los dataframes
-lista_dataframes = []
-
-# Iterar sobre la lista de archivos csv
-for archivo in archivos_csv:
-    # Leer cada archivo csv y agregarlo a la lista de dataframes
-    lista_dataframes.append(pd.read_csv(archivo))
-
-# Usar el primer DataFrame como base
-df_base = lista_dataframes[0]
-
-# Iterar sobre los demás DataFrames y combinarlos con el DataFrame base
-for df in lista_dataframes[1:]:
-    df_base = pd.merge(df_base, df, on=['id_archivo', 'id_archivo'], how='inner')
+df_estructuras = pd.read_csv(url1)
+df_memorias = pd.read_csv(url2)
 
 # Añadir una opción para seleccionar el modelo de openai a utilizar
 model_option = st.sidebar.selectbox(
@@ -49,18 +34,17 @@ model_option = st.sidebar.selectbox(
     ('gpt-3.5-turbo-1106','gpt-4','gpt-4-1106-preview')
 )
 
-def generate_response(df_base, user_query):
+def generate_response(df_estructuras, df_memorias, user_query):
  # Crear un objeto `ChatOpenAI` con la configuración deseada.
  llm = ChatOpenAI(temperature=0, model=model_option, openai_api_key=openai_api_key, streaming=True)
- df_base = df_base
- # Crear un objeto `PromptTemplate` con el formato de la respuesta deseada.
+  # Crear un objeto `PromptTemplate` con el formato de la respuesta deseada.
  _DEFAULT_TEMPLATE = """
   Dada una consulta del usuario {dialect}
-Sólo utiliza la información de df_base
-  1. Consulta los datos similares en df_base
+Sólo utiliza la información de df_estructuras y df_memorias
+  1. Consulta los datos similares en df_estructuras
   2. Devuelve una respuesta en español que incluya:
-     - Referencias a proyectos con las condiciones exactas a la consulta de usuario en df_base con id_archivo
-     - Referencias a proyectos con las condiciones similares a la consulta de usuario en df_base con id_archivo
+     - Referencias a proyectos con las condiciones exactas a la consulta de usuario en df_estructuras con id_archivo y su url de ubicación de df_memorias
+     - Referencias a proyectos con las condiciones similares a la consulta de usuario en df_estructuras con id_archivo  y su url de ubicación de df_memorias
 
   Respuesta {output}
   Pregunta {input}
@@ -69,7 +53,8 @@ Sólo utiliza la información de df_base
   # Crear un agente `pandas_df_agent` que usa el modelo de lenguaje y el DataFrame base.
  pandas_df_agent = create_pandas_dataframe_agent(
         llm,
-        df_base,
+        df_estructuras,
+        df_memorias,
         verbose=True,
         agent_type=AgentType.OPENAI_FUNCTIONS,
         prompt_template=PROMPT,
@@ -77,8 +62,8 @@ Sólo utiliza la información de df_base
         handle_parsing_errors=True,
   )
  
- response = pandas_df_agent({"input": user_query}, {"dialect": _DEFAULT_TEMPLATE}, include_run_info=True)
- result = generate_response(df_base, user_query)
+ #response = pandas_df_agent({"input": user_query}, {"dialect": _DEFAULT_TEMPLATE}, include_run_info=True)
+ result = generate_response(df_estructuras, df_memorias, user_query)
  return st.success(result)
 
 
@@ -86,8 +71,8 @@ Sólo utiliza la información de df_base
 caracteristicas_estructura = st.text_input('Ingresa las características para la búsqueda:', placeholder = 'Escribe tu consulta aquí ...')
 
 # Agrega más información a la solicitud para una respuesta robusta
-texto_ad1 = "Limítate a siempre actuar como buscador en la base de datos, además referencia proyectos usando id_archivo con las condiciones exactas en df_base a la siguiente consulta de usuario :\n"
-texto_ad2 = "\n O referencia proyectos usando id_archivo con alguna condición similar a la consulta de usuario en df_base y describe las características de cada proyecto referenciado"
+texto_ad1 = "Limítate a siempre actuar como buscador en la base de datos, además referencia proyectos usando id_archivo con las condiciones exactas en df_estructuras a la siguiente consulta de usuario :\n"
+texto_ad2 = "\n O referencia proyectos usando id_archivo con alguna condición similar a la consulta de usuario en df_estructuras, referencia por id_archivo, por url y describe las características de cada proyecto referenciado"
 user_query = texto_ad1 + caracteristicas_estructura + texto_ad2
 
 openai_api_key = st.sidebar.text_input('Inserte su OpenAI API Key', type='password', disabled=not (user_query))
@@ -96,4 +81,4 @@ if not openai_api_key.startswith('sk-'):
  st.warning('Por favor ingrese su llave OpenAI API!', icon='⚠')
 if openai_api_key.startswith('sk-'):
  st.header('Sugerencia')
- generate_response(df_base, user_query)
+ 
