8eea5a94f9888a3bb78ee8ec808c2d3bf5231fd8
Jefferson Ramos <151884695+Jeffracu@users.noreply.github.com>
2023-12-19 07:49:45 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,8 +26,9 @@ url1 = "https://github.com/Jeffracu/DataBase-SearchGPT/blob/master/informacionEs
 url2 = "https://github.com/Jeffracu/DataBase-SearchGPT/blob/master/archivosMemorias.csv"
 
 
-df_estructuras = pd.read_csv(url1, sep='your_delimiter', encoding="utf-8", error_bad_lines=False)
-df_memorias = pd.read_csv(url2, sep='your_delimiter', encoding="utf-8", error_bad_lines=False)
+df_estructuras = pd.read_csv(url1, sep=',', encoding="utf-8", error_bad_lines=False)
+df_memorias = pd.read_csv(url2, sep=',', encoding="utf-8", error_bad_lines=False)
+df_db = pd.merge(df_estructuras, df_memorias, on='id_archivo', how='outer')
 
 # Añadir una opción para seleccionar el modelo de openai a utilizar
 model_option = st.sidebar.selectbox(
@@ -35,13 +36,13 @@ model_option = st.sidebar.selectbox(
     ('gpt-3.5-turbo-1106','gpt-4','gpt-4-1106-preview')
 )
 
-def generate_response(df_estructuras, df_memorias, user_query):
+def generate_response(df_db, user_query):
  # Crear un objeto `ChatOpenAI` con la configuración deseada.
  llm = ChatOpenAI(temperature=0, model=model_option, openai_api_key=openai_api_key, streaming=True)
  # Crear un objeto `PromptTemplate` con el formato de la respuesta deseada.
  _DEFAULT_TEMPLATE = """
  Dada una consulta del usuario {dialect}
- Sólo utiliza la información de df_estructuras y df_memorias
+ Sólo utiliza la información de df_db que integra df_estructuras y df_memorias 
  1. Consulta los datos similares en df_estructuras
  2. Devuelve una respuesta en español que incluya:
    - Referencias a proyectos con las condiciones exactas a la consulta de usuario en df_estructuras con id_archivo y su url de ubicación de df_memorias
@@ -54,8 +55,7 @@ def generate_response(df_estructuras, df_memorias, user_query):
  # Crear un agente `pandas_df_agent` que usa el modelo de lenguaje y el DataFrame base.
  pandas_df_agent = create_pandas_dataframe_agent(
     llm,
-    df_estructuras,
-    df_memorias,
+    df_db,
     verbose=True,
     agent_type=AgentType.OPENAI_FUNCTIONS,
     prompt_template=PROMPT,
@@ -73,8 +73,8 @@ def generate_response(df_estructuras, df_memorias, user_query):
 caracteristicas_estructura = st.text_input('Ingresa las características para la búsqueda:', placeholder = 'Escribe tu consulta aquí ...')
 
 # Agrega más información a la solicitud para una respuesta robusta
-texto_ad1 = "Limítate a siempre actuar como buscador en la base de datos, además referencia proyectos usando id_archivo con las condiciones exactas en df_estructuras a la siguiente consulta de usuario :\n"
-texto_ad2 = "\n O referencia proyectos usando id_archivo con alguna condición similar a la consulta de usuario en df_estructuras, referencia por id_archivo, por url y describe las características de cada proyecto referenciado"
+texto_ad1 = "Limítate a siempre actuar como buscador en la base de datos, además referencia proyectos usando id_archivo con las condiciones exactas de df_db en df_estructuras a la siguiente consulta de usuario :\n"
+texto_ad2 = "\n O referencia proyectos usando id_archivo con alguna condición similar a la consulta de usuario de df_db en df_estructuras, referencia por id_archivo, por url y describe las características de cada proyecto referenciado"
 user_query = texto_ad1 + caracteristicas_estructura + texto_ad2
 
 openai_api_key = st.sidebar.text_input('Inserte su OpenAI API Key', type='password', disabled=not (user_query))
@@ -83,4 +83,4 @@ if not openai_api_key.startswith('sk-'):
  st.warning('Por favor ingrese su llave OpenAI API!', icon='⚠')
 if openai_api_key.startswith('sk-'):
  st.header('Sugerencia')
- generate_response(df_estructuras, df_memorias, user_query)
+ generate_response(df_db, user_query)
